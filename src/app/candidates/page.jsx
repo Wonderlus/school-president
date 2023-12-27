9831096ae964c31b88be2e7e32a1429ae0e35151
Wonderlus <iliyawhitelion@gmail.com>
2023-12-28 00:40:15 +0300

    complete design candidates and vote pages

--- src/app/candidates/page.jsx
+++ src/app/candidates/page.jsx
@@ -4,65 +4,53 @@ import styles from "./page.module.css";
 const Candidates = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.candidate}>
-                <Image
-                    className={styles.img}
-                    src={"/1.jpg"}
-                    alt="Березкин Дмитрий Петрович"
-                    width={700}
-                    height={500}
-                />
-                <h1 className={styles.name}>Березкин Дмитрий Петрович</h1>
-                <p className={styles.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                </p>
-            </div>
-            <div className={styles.candidate}>
-                <Image
-                    className={styles.img}
-                    src={"/2.jpg"}
-                    alt="Хиневич Максим Дмитриевич"
-                    width={700}
-                    height={500}
-                />
-                <h1 className={styles.name}>Хиневич Максим Дмитриевич</h1>
-                <p className={styles.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                </p>
-            </div>
-            <div className={styles.candidate}>
-                <Image
-                    className={styles.img}
-                    src={"/3.jpg"}
-                    alt="Касесалу Герман Янович"
-                    width={700}
-                    height={500}
-                />
-                <h1 className={styles.name}>Касесалу Герман Янович</h1>
-                <p className={styles.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                </p>
+            <h1 className={styles.title}>Кандидаты</h1>
+            <div className={styles.candidates}>
+                <div className={styles.candidate}>
+                    <Image
+                        className={styles.img}
+                        src={"/1.png"}
+                        alt="Березкин Дмитрий Петрович"
+                        width={700}
+                        height={500}
+                    />
+                    <div className={styles.nameBlock}>
+                        <h1 className={styles.name}>
+                            Березкин Дмитрий Петрович
+                        </h1>
+                        <div className={styles.number}>01</div>
+                    </div>
+                </div>
+
+                <div className={styles.candidate}>
+                    <Image
+                        className={styles.img}
+                        src={"/2.png"}
+                        alt="Хиневич Максим Дмитриевич"
+                        width={700}
+                        height={500}
+                    />
+                    <div className={styles.nameBlock}>
+                        <div className={styles.number}>02</div>
+                        <h1 className={styles.name}>
+                            Хиневич максим дмитриевич
+                        </h1>
+                    </div>
+                </div>
+
+                <div className={styles.candidate}>
+                    <Image
+                        className={styles.img}
+                        src={"/3.png"}
+                        alt="Касесалу Герман Янович"
+                        width={700}
+                        height={500}
+                    />
+                    <div className={styles.nameBlock}>
+                        <h1 className={styles.name}>Касесалу герман янович</h1>
+                        <div className={styles.number}>03</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
